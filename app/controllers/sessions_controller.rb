7b65120bd12bed9e96b979696b85c756476a00d5
y-natsuki <n-yoshida@starx.co.jp>
2025-05-12 10:25:24 +0900

    Implement advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,10 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      reset_session      # ログインの直前に必ずこれを書くこと
+      reset_session
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      #remember user
+      # ログインの直前に必ずこれを書くこと
       log_in user
       redirect_to user
       # ユーザーログイン後にユーザー情報のページにリダイレクトする
@@ -17,6 +20,7 @@ class SessionsController < ApplicationController
 
   def destroy
     log_out
+    log_out if logged_in?
     redirect_to root_url, status: :see_other
   end
 end
